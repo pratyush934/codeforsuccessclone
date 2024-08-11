4f6d28b648299cffbdd2fdcf84cc768e06bf8652
Pratyush <oggycandle786@gmail.com>
2024-08-11 23:52:19 +0530

    Adding something imp

--- src/app/(auth)/dashboard/layout.tsx
+++ src/app/(auth)/dashboard/layout.tsx
@@ -6,6 +6,7 @@ const DashBoardLayout = ({children} : {children: React.ReactNode}) => {
         <div className='mt-10 '>
         {children}
         </div>
+        {/* adding comment */}
     </div>
   )
 }
