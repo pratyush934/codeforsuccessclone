ba99e37eb0da3389ad46812987ac483fb8c0234a
Pratyush <oggycandle786@gmail.com>
2024-08-14 19:37:25 +0530

    Adding sidebar

--- src/components/layouts/CoreLayout.tsx
+++ src/components/layouts/CoreLayout.tsx
@@ -1,16 +1,23 @@
 import { ReactNode } from "react";
 import AppBar from "../AppBar";
-import Sidebar from "../Sidebar";
+import { SidebarDemo } from "../Sidebar";
 
 const CoreLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex flex-col h-screen w-screen overflow-hidden">
+    // <div className="flex flex-col h-screen w-screen overflow-hidden">
+    //   <AppBar />
+    //   <div className="flex flex-1 overflow-hidden">
+    //     <main className="flex-1 bg-black p-4 overflow-auto">
+    //       <div className="h-full w-full flex flex-col items-center">
+    //         {children}
+    //       </div>
+    //     </main>
+    //   </div>
+    // </div>
+    <div className="flex flex-col h-screen w-screen">
       <AppBar />
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 bg-black p-4 overflow-auto">
-          <div className="h-full w-full flex flex-col items-center">{children}</div>
-        </main>
+      <div className="flex flex-1 h-full w-screen">
+        <SidebarDemo>{children}</SidebarDemo>
       </div>
     </div>
   );
