375d225ae28fac0042adc4bf2359c7987fdb652c
Pratyush <oggycandle786@gmail.com>
2024-08-16 23:59:46 +0530

    A

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -86,6 +86,7 @@ export function SidebarDemo({ children }: { children: React.ReactNode }) {
         </SidebarBody>
       </Sidebar>
       <Dashboard> {children}</Dashboard>
+      {/* Greate */}
     </div>
   );
 }
